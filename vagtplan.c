5d225b39bccd83382d671b634856a523fb67b9d5
Retr0m4n <lekermand@gmail.com>
2025-11-19 15:48:09 +0100

    Update vagtplan.c

--- vagtplan.c
+++ vagtplan.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-struct user{
+struct user
+{
 char username[20];
 char password[20];
 char pref_days[20][20];
@@ -12,9 +13,10 @@ typedef struct user user;
 
 int main (void){
 
-//
 
+user User_1 = {"VikRas22", "asdkasd", "Mandag" "Tirsdag" "Onsdag" "Torsdag", "Mandag" "Tirsdag" "Onsdag" "Torsdag"};
 
+printf("%s",User_1.username);
 
 
 
