920357ac00dee633bdca2e0a665f70a2e79d7648
Frederik-Langendorf <frederik.langendorf@gmail.com>
2025-11-19 15:38:32 +0100

    Udkast 0
    Added a user struct to manage user data.

--- vagtplan.c
+++ vagtplan.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+
+struct user{
+char username[20];
+char password[20];
+char pref_days[20][20];
+char assinged_shifts[20][20];
+};
+typedef struct user user;
+
 
 int main (void){
 
-//test ændring 
+//
 
 
 
