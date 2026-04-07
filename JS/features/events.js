7a908bb382a932edd7ab5350688b0c110dfa0bf4
sabrinkhan06 <sabrin.sk@hotmail.com>
2026-04-07 12:37:10 +0200

    Create events.js

--- JS/features/events.js
+++ JS/features/events.js
@@ -1,1 +1,28 @@
+const container = document.getElementById("admin-panel");
+export function renderCreateEventForm() {
+     container.innerHTML = `
+        <h2>Create Event</h2>
+        <form id="event-form">
+            <input type="text" id="title" placeholder="Title" required />
+            <input type="date" id="date" required />
+            <input type="text" id="location" placeholder="Location" required />
+            <textarea id="description" placeholder="Description" required></textarea>
+            <button type="submit">Create Event</button>
+        </form>
+    `;
+
+     document
+        .getElementById("event-form")
+        .addEventListener("submit", handleCreateEvent);
+}
+
+export function validateEventForm(data) {
+    return (
+        data.title &&
+        data.date &&
+        data.location &&
+        data.description
+    );
+}
+
 
